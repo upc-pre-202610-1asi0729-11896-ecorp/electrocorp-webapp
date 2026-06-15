e996955b062c70d6a1976d6487a9b2f4d1855c69
Msa_ware <73900517+Msa-ware@users.noreply.github.com>
2026-06-15 16:41:04 -0500

    feat(iam): connect sign-in infrastructure

---
+++ src/app/iam/infrastructure/api/auth-api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { API_BASE_URL } from '../../../shared/infrastructure/api/api-config';
+
+import { SignInResource } from '../resources/sign-in.resource';
+import { AuthResponse } from '../responses/auth.response';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthApiService {
+  private readonly authUrl = `${API_BASE_URL}/auth`;
+
+  constructor(private readonly http: HttpClient) {}
+
+  signIn(resource: SignInResource): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.authUrl}/sign-in`, resource);
+  }
+}
