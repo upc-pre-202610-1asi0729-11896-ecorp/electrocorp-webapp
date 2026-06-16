15b6e99838dbd46b642fdf9d15ba28cd0d6df5ea
Jean Loa <loarojas1@gmail.com>
2026-06-15 19:22:11 -0500

    feat(workplace): restore space management

--- src/app/workplace/application/dtos/create-location.dto.ts
+++
@@ -1,9 +0,0 @@
-import { LocationType } from '../../domain/model/location.entity';
-
-export interface CreateLocationDto {
-  name: string;
-  address: string;
-  city: string;
-  country: string;
-  type: LocationType;
-}
--- src/app/workplace/infrastructure/responses/device-assignment.response.ts
+++ src/app/workplace/infrastructure/responses/device-assignment.response.ts
@@ -1,9 +1,9 @@
 import { BaseResponse } from '../../../shared/infrastructure/responses/base.response';
 
 export interface DeviceAssignmentResponse extends BaseResponse<number> {
-  deviceId: number;
   userId: number;
+  deviceId: number;
   locationId: number;
-  roomId: number | null;
+  roomId?: number | null;
   assignedAt: string;
 }
